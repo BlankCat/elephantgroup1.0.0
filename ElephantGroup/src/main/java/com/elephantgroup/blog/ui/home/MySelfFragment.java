16af701461ccc4ada208e96e135395950eeae9d3
wkp91 <563150732@qq.com>
2016-11-21 11:18:52 +0800

    我的页面  设置页面

--- ElephantGroup/src/main/java/com/elephantgroup/blog/ui/home/MySelfFragment.java
+++ ElephantGroup/src/main/java/com/elephantgroup/blog/ui/home/MySelfFragment.java
@@ -1,17 +1,22 @@
 package com.elephantgroup.blog.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.elephantgroup.blog.R;
 import com.elephantgroup.blog.custom.AlwaysMarqueeTextView;
 import com.elephantgroup.blog.ui.base.BaseFragment;
+import com.elephantgroup.blog.ui.setting.SettingUI;
+import com.elephantgroup.blog.util.Utils;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 我的页面
@@ -23,6 +28,10 @@ public class MySelfFragment extends BaseFragment {
     AlwaysMarqueeTextView appTitle;
     @Bind(R.id.app_back)
     ImageView appBack;
+    @Bind(R.id.nickTv)
+    TextView nickTv;//昵称
+    @Bind(R.id.singTv)
+    TextView singTv;//签名
     private boolean isFirstLoadedData;
     private View view;
 
@@ -50,6 +59,22 @@ public class MySelfFragment extends BaseFragment {
         appTitle.setVisibility(View.VISIBLE);
         appTitle.setText(getString(R.string.it_elephant));
         appBack.setVisibility(View.GONE);
+    }
+
+    @OnClick({R.id.myMark,R.id.labelManger,R.id.setting})
+    private void clickMethod(View view){
+        switch (view.getId()){
+            case R.id.myMark://我的收藏
+
+                break;
+            case R.id.labelManger://标签管理
+
+                break;
+            case R.id.setting://设置
+                startActivity(new Intent(getActivity(), SettingUI.class));
+                Utils.openNewActivityAnim(getActivity(), false);
+                break;
+        }
     }
 
     @Override
---
+++ ElephantGroup/src/main/java/com/elephantgroup/blog/ui/setting/SettingUI.java
@@ -0,0 +1,19 @@
+package com.elephantgroup.blog.ui.setting;
+
+import com.elephantgroup.blog.ui.base.BaseFragmentActivity;
+
+/**
+ * 设置页面
+ * Created on 2016/11/21.
+ */
+public class SettingUI extends BaseFragmentActivity {
+    @Override
+    protected void setContentView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
